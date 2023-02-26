b91bd5ca73224eca5268e907fde2ef1447a0ff70
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-02-26 12:56:23 -0700

    Added ActionBar buttons
    Added ActionBar buttons to the QR Code Information page

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationFragment.java
@@ -2,6 +2,8 @@ package com.cmput301w23t00.qrquest.ui.library.qrcodeinformation;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.cmput301w23t00.qrquest.R;
 import com.cmput301w23t00.qrquest.databinding.FragmentQrcodeinformationBinding;
 
 public class QRCodeInformationFragment extends Fragment {
@@ -26,8 +29,21 @@ public class QRCodeInformationFragment extends Fragment {
 
         final TextView textView = binding.textQRCodeInformation;
         qrCodeInformationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.top_nav_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
 
     @Override
     public void onDestroyView() {
