4b07e85d2c443fe7f861bb22e162007082f071bc
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-02-26 12:56:23 -0700

    Added ActionBar buttons
    Added ActionBar buttons to the QR Code Information page

--- app/src/main/java/com/cmput301w23t00/qrquest/MainActivity.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/MainActivity.java
@@ -2,8 +2,6 @@ package com.cmput301w23t00.qrquest;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cmput301w23t00.qrquest.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -26,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_map, R.id.navigation_qrcode_library, R.id.navigation_add_qrcode,
+                R.id.navigation_map, R.id.navigation_qrcode_information, R.id.navigation_add_qrcode,
                 R.id.navigation_search, R.id.navigation_profile)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
