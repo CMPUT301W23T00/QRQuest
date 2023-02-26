39ecdd2b90ee9487afe1f080b99cc91172b332bc
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-02-25 18:12:48 -0700

    Created QR Code information Fragment
    Not done yet

---
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationViewModel.java
@@ -0,0 +1,19 @@
+package com.cmput301w23t00.qrquest.ui.library.qrcodeinformation;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class QRCodeInformationViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public QRCodeInformationViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is QR Code Information fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
