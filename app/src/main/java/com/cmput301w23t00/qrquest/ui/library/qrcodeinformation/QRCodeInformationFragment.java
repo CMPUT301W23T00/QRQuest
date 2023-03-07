694daebdff0295ba5de4ec221a083fcdfbda81e2
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-03-06 17:54:53 -0700

    Added comments and edited UI
    Added comments to the classes used in the QR Code information page

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationFragment.java
@@ -15,12 +15,26 @@ import androidx.lifecycle.ViewModelProvider;
 import com.cmput301w23t00.qrquest.R;
 import com.cmput301w23t00.qrquest.databinding.FragmentQrcodeinformationBinding;
 
+
+/**
+ * The class  QR code information fragment extends fragment
+ */
 public class QRCodeInformationFragment extends Fragment {
 
     private FragmentQrcodeinformationBinding binding;
 
+    /**
+     *
+     * On create view
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         QRCodeInformationViewModel qrCodeInformationViewModel =
                 new ViewModelProvider(this).get(QRCodeInformationViewModel.class);
 
@@ -33,20 +47,44 @@ public class QRCodeInformationFragment extends Fragment {
         return root;
     }
 
+
+    /**
+     *
+     * On create
+     *
+     * @param savedInstanceState  the saved instance state
+     */
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
 
     @Override
+
+    /**
+     *
+     * On create options menu
+     *
+     * @param menu  the menu
+     * @param inflater  the inflater
+     */
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
         // adds buttons to the top navigation bar for navigation and to delete the QR Code
         inflater.inflate(R.menu.qr_code_information_top_nav_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
+
+    /**
+     *
+     * On destroy view
+     *
+     */
     public void onDestroyView() {
+
         super.onDestroyView();
         binding = null;
     }
