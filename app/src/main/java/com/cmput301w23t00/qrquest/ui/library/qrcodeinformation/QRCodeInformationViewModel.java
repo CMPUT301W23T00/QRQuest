783728429bb0165550d34840d0996cacfe4fb7f3
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-03-06 17:54:53 -0700

    Added comments and edited UI
    Added comments to the classes used in the QR Code information page

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationViewModel.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/library/qrcodeinformation/QRCodeInformationViewModel.java
@@ -4,16 +4,35 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+
+/**
+ * The class  QR code information view model extends view model
+ */
 public class QRCodeInformationViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+
+    /**
+     *
+     *  QR code information view model
+     *
+     */
     public QRCodeInformationViewModel() {
+
         mText = new MutableLiveData<>();
         mText.setValue("This is QR Code Information Description");
     }
 
+
+    /**
+     *
+     * Gets the text
+     *
+     * @return the text
+     */
     public LiveData<String> getText() {
+
         return mText;
     }
 }
