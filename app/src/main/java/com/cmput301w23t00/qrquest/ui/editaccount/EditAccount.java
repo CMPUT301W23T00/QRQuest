8e7e834745eb01077e21d247e856ba90aebf5632
alifbink-ualberta <alifbink@ualberta.ca>
2023-03-08 15:38:02 -0700

    Set content view for EditAccount and CreateAccount.

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/editaccount/EditAccount.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/editaccount/EditAccount.java
@@ -3,9 +3,12 @@ package com.cmput301w23t00.qrquest.ui.editaccount;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cmput301w23t00.qrquest.R;
+
 public class EditAccount extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_account);
     }
 }
