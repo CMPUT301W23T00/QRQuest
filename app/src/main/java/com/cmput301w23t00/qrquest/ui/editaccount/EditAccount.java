9f8c0b861a518b08839c056ada218dd6c3b32cd6
alifbink-ualberta <alifbink@ualberta.ca>
2023-03-08 14:07:46 -0700

    Added Java activity file for creating and editting account.

---
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/editaccount/EditAccount.java
@@ -0,0 +1,11 @@
+package com.cmput301w23t00.qrquest.ui.editaccount;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditAccount extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
