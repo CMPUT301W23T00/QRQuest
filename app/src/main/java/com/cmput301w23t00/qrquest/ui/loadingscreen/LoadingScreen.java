c06c89b29580165cfcce5290390839184e30976f
alifbink-ualberta <alifbink@ualberta.ca>
2023-03-09 02:02:57 -0700

    Added comments in LoadingScreen.java

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/loadingscreen/LoadingScreen.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/loadingscreen/LoadingScreen.java
@@ -20,21 +20,26 @@ public class LoadingScreen extends AppCompatActivity {
         // Instead of getSupportActionBar().hide(); pre-req is that Support Action Bar must be set.
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        // uid: takes uid from firebase
         String uid = null;
 
+        // Method to invoke loading time; second variable is time in milliseconds.
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // if uid is not present, goes to CreateAccount.
                 if (uid.equals(null)){
                     Intent intentNoUID = new Intent(LoadingScreen.this, CreateAccount.class);
                     startActivity(intentNoUID);
                 }
+                // if uid is present, goes to MainActivity.
                 else {
                     Intent intentWithUID = new Intent(LoadingScreen.this, MainActivity.class);
                     startActivity(intentWithUID);
                 }
+                // finish() ends this activity (user won't be able to go back to loading screen).
                 finish();
             }
-        }, 3000);
+        }, 5000);
     }
 }
