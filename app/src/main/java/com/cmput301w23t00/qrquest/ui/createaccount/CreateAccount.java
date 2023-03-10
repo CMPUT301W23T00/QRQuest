095d86f84b771a5c8cb1f67bf16c4334fae54aa7
alifbink-ualberta <alifbink@ualberta.ca>
2023-03-10 00:41:11 -0700

    Added element identifiers.

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/createaccount/CreateAccount.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/createaccount/CreateAccount.java
@@ -1,18 +1,61 @@
 package com.cmput301w23t00.qrquest.ui.createaccount;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
 
+import com.cmput301w23t00.qrquest.MainActivity;
 import com.cmput301w23t00.qrquest.R;
 
 public class CreateAccount extends AppCompatActivity {
+
+    EditText addNameField, addEmailField, addPhoneField;
+    Button addCreateButton;
+    ImageView addProfileImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+        addNameField = findViewById(R.id.addNameField);
+        addEmailField = findViewById(R.id.addEmailField);
+        addPhoneField = findViewById(R.id.addPhoneField);
+        addCreateButton = findViewById(R.id.addCreateButton);
+        addProfileImage = findViewById(R.id.addProfileImage);
+
+        addProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // pop up upload option
+                // send photo to db
+                // update profile image view
+            }
+        });
+
+        addCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // manipulate name, email, phone, profile image in db
+
+
+                // Goes back to home screen.
+                Intent intentAccountCreated = new Intent(CreateAccount.this, MainActivity.class);
+                startActivity(intentAccountCreated);
+
+                finish();
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/cmput301w23t00/qrquest/ui/editaccount/EditAccount.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/editaccount/EditAccount.java
@@ -1,14 +1,57 @@
 package com.cmput301w23t00.qrquest.ui.editaccount;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cmput301w23t00.qrquest.R;
 
 public class EditAccount extends AppCompatActivity {
+
+    EditText editNameField, editAboutMeField, editEmailField, editPhoneField;
+    Button editCancelButton, editConfirmButton;
+    ImageView editProfileImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_account);
+
+        editNameField = findViewById(R.id.editNameField);
+        editAboutMeField = findViewById(R.id.editAboutMeField);
+        editEmailField = findViewById(R.id.editEmailField);
+        editPhoneField = findViewById(R.id.editPhoneField);
+        editCancelButton = findViewById(R.id.editCancelButton);
+        editConfirmButton = findViewById(R.id.editConfirmButton);
+        editProfileImage = findViewById(R.id.editProfileImage);
+
+        editCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go back to settings.
+                // IMPORTANT: add settings class.
+                Intent intentEditCancelled = new Intent(EditAccount.this, .class);
+                startActivity(intentEditCancelled);
+                finish();
+            }
+        });
+
+        editConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // manipulate name, email, phone, about me, profile image in db
+
+                // Go back to settings.
+                // IMPORTANT: add settings class.
+                Intent intentEditConfirmed = new Intent(EditAccount.this, .class);
+                startActivity(intentEditConfirmed);
+                finish();
+            }
+        });
     }
 }
