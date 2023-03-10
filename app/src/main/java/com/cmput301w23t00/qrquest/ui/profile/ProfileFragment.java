fdba3887fb5c48581ba6a308fbf43da299507bee
Dsorken <113465018+Dsorken@users.noreply.github.com>
2023-03-09 21:43:41 -0700

    initial commit

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/profile/ProfileFragment.java
@@ -1,15 +1,21 @@
 package com.cmput301w23t00.qrquest.ui.profile;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.cmput301w23t00.qrquest.R;
 import com.cmput301w23t00.qrquest.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -25,8 +31,24 @@ public class ProfileFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textProfile;
+        setHasOptionsMenu(true);
+
         profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.profile_settings_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings_button) {
+            return true;
+        }
+        else return super.onOptionsItemSelected(item);
     }
 
     @Override
