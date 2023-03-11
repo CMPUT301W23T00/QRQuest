69a22d50fe76c06990e92b841354e5e18b8300f7
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-03-11 00:27:53 -0700

    Add QR Code Scanner

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/addqrcode/AddQRCodeFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/addqrcode/AddQRCodeFragment.java
@@ -1,37 +1,74 @@
 package com.cmput301w23t00.qrquest.ui.addqrcode;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.cmput301w23t00.qrquest.databinding.FragmentAddqrcodeBinding;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.cmput301w23t00.qrquest.R;
+import com.google.zxing.Result;
 
 public class AddQRCodeFragment extends Fragment {
+    private CodeScanner mCodeScanner;
 
-    private FragmentAddqrcodeBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final Activity activity = getActivity();
+        View root = inflater.inflate(R.layout.fragment_addqrcode, container, false);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        AddQRCodeViewModel addQRCodeViewModel =
-                new ViewModelProvider(this).get(AddQRCodeViewModel.class);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.CAMERA }, 100);
+        }
 
-        binding = FragmentAddqrcodeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            CodeScannerView scannerView = root.findViewById(R.id.scanner_view);
+            mCodeScanner = new CodeScanner(activity, scannerView);
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull final Result result) {
+                    // call new fragment to take picture
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
+                }
+            });
+        }
 
-        final TextView textView = binding.textAddQRCode;
-        addQRCodeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
