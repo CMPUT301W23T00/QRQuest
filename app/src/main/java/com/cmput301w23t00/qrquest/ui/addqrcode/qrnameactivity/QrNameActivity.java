2e207cd12df7ab3b53e11210c2eeffcfffaf5b40
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-03-12 11:16:54 -0600

    Revert "Need to fix Click"
    This reverts commit df6f176656d497811eb69365924e843b3dc91aa5.

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/addqrcode/qrnameactivity/QrNameActivity.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/addqrcode/qrnameactivity/QrNameActivity.java
@@ -1,26 +1,19 @@
 package com.cmput301w23t00.qrquest.ui.addqrcode.qrnameactivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.FileProvider;
 
 import com.cmput301w23t00.qrquest.MainActivity;
 import com.cmput301w23t00.qrquest.R;
@@ -29,12 +22,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
@@ -42,73 +30,30 @@ public class QrNameActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private PreviewView previewView;
-    private ImageCapture imageCapture;
-    private File outputDirectory;
-    private File photoFile;
+    EditText addNameField, addEmailField, addPhoneField;
+    Button addCreateButton;
+    ImageView addProfileImage;
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-
-        // Get the directory where the file should be saved
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        if (storageDir == null || !storageDir.exists()) {
-            storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath());
-        }
-
-        // Create the file
-        File imageFile = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        String currentPhotoPath = imageFile.getAbsolutePath();
-        return imageFile;
-    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_addqrcode);
+        setContentView(R.layout.activity_takephoto);
 
         Objects.requireNonNull(getSupportActionBar()).setTitle("QR Name");
 
         Intent qrCodeIntent = getIntent();
         String qrCodeData = Objects.requireNonNull(qrCodeIntent).getStringExtra("qrCodeData");
 
-        Button frame =  findViewById(R.id.clickbutton);
+        ImageView imgview =  findViewById(R.id.main_backgroundImage);
 
-        frame.setOnClickListener(new View.OnClickListener() {
+        imgview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                // Ensure that there's a camera activity to handle the intent
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    // Create the File where the photo should go
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        // Error occurred while creating the File
-                        ex.printStackTrace();
-                    }
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        Uri photoURI;
-                        photoURI = FileProvider.getUriForFile(QrNameActivity.this,
-                                "com.cmput301w23t00.qrquest.fileprovider",
-                                photoFile);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
-                }
+                ImageCapture imageCapture =
+                        new ImageCapture.Builder()
+                                .setTargetRotation(view.getDisplay().getRotation())
+                                .build();
             }
-        });
+        }
     }
 }
-
