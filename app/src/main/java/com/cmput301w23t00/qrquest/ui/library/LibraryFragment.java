4b37e47c5eb06e0a9c199d349db008ddd61ea1cf
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-03-11 22:32:15 -0700

    QR code info page
    Need to uncomment lines when library fragment is complete

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/library/LibraryFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/library/LibraryFragment.java
@@ -1,36 +1,20 @@
 package com.cmput301w23t00.qrquest.ui.library;
 
-import static androidx.navigation.Navigation.findNavController;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
-import com.cmput301w23t00.qrquest.R;
 import com.cmput301w23t00.qrquest.databinding.FragmentLibraryBinding;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 public class LibraryFragment extends Fragment {
 
     private FragmentLibraryBinding binding;
-    private ListView QRList;
-    private ArrayAdapter<LibraryQRCode> QRAdapter;
-    private ArrayList<LibraryQRCode> dataList;
-    private int mSelectedItemIndex;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,44 +24,8 @@ public class LibraryFragment extends Fragment {
         binding = FragmentLibraryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // QR Stats button
-        Button viewQrStats = binding.viewPersonalQrStatsButton;
-        viewQrStats.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavController navController = findNavController(view);
-                //navController.navigate(R.id.action_navigation_qrcode_library_to_qrCodeSummaryStatisticsFragment2);
-            }
-        });
-
-        // QR Code List
-        QRList = binding.libraryQrCodesList;
-        dataList = new ArrayList<>();
-        String[] QRDatas = {"t1", "t2", "t3"};
-        Integer[] QRScores = {1,2,3};
-        Date[] QRDates = {new Date(), new Date(), new Date()};
-        for (int i = 0; i < QRDatas.length; i++) {
-            dataList.add(new LibraryQRCode(QRDatas[i], QRScores[i], QRDates[i]));
-        }
-        QRAdapter = new LibraryQRCodeAdapter((Context) getActivity(), dataList);
-        QRList.setAdapter(QRAdapter);
-
-        QRList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
-                mSelectedItemIndex = index;
-                LibraryQRCode qrCode = dataList.get(index);
-
-                Bundle bundle = new Bundle();
-                bundle.putParcelable("selectedQRCode", qrCode);
-
-                Navigation.findNavController(view).navigate(R.id.action_libraryFragment_to_qrCodeInformationFragment, bundle);
-            }
-        });
-
-
-        //final TextView textView = binding.textLibrary;
-        //libraryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textLibrary;
+        libraryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
