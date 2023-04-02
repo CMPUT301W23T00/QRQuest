9213a87008b7365c8112c76d7248c29a88c9069b
Ashwin <ashwinshreekumar123@gmail.com>
2023-04-02 14:02:50 -0600

    Add basic search view

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/search/SearchFragment.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/search/SearchFragment.java
@@ -1,6 +1,11 @@
 package com.cmput301w23t00.qrquest.ui.search;
 
+import static android.content.ContentValues.TAG;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +15,24 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.cmput301w23t00.qrquest.R;
 import com.cmput301w23t00.qrquest.databinding.FragmentSearchBinding;
+import com.cmput301w23t00.qrquest.ui.addqrcode.QRCodeProcessor;
+import com.cmput301w23t00.qrquest.ui.map.QRCodeSuggestions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * SearchFragment is an template class not yet with fully implemented functionality
