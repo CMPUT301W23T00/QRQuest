43a55343b4f6f6ab57c7e1fdf5a0ba1ed93d6c83
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2023-04-03 13:47:40 -0600

    JavaDocs for LoadingScreen.

--- app/src/main/java/com/cmput301w23t00/qrquest/ui/createaccount/CreateAccount.java
+++ app/src/main/java/com/cmput301w23t00/qrquest/ui/createaccount/CreateAccount.java
@@ -34,6 +34,9 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
+/**
+ *  This activity creates a user account and instantiates each user document in the Firestore database.
+ */
 public class CreateAccount extends AppCompatActivity implements EditAvatarDialogListener {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -154,6 +157,11 @@ public class CreateAccount extends AppCompatActivity implements EditAvatarDialog
         });
     }
 
+
+    /**
+     * Updates image displayed in Profile Picture to avatar referred by 'id'.
+     * @param id is the avatarId of the user.
+     */
     @Override
     public void updateProfilePicture(int id) {
         profilePictureID = id;
